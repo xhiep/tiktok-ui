bf0301ca1aac917056acc941c0dd3449fc563caf
xhiep <xhhiep@gmail.com>
2023-03-01 17:48:18 +0700

    Xây dựng UI phần Header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,14 +1,27 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
-
+import images from '~/assets/images';
+import { IoMdCloseCircle } from 'react-icons/io';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                {/* {logo} */}
-                {/* {logo} */}
+                <img src={images.logo} alt="tiktok" />
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <IoMdCloseCircle />
+                    </button>
+                    <AiOutlineLoading3Quarters className={cx('loading')} />
+                    <button className={cx('search-btn')}>
+                        <BiSearch className={cx('search-icon')} />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
             </div>
         </header>
     );

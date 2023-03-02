b59bae2320bcf86d8b6c86264ff2abef4b754d64
xhiep <xhhiep@gmail.com>
2023-03-03 02:12:02 +0700

    Xây dựng UI phần Header #4

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,5 @@
 const images = {
     logo: require('~/assets/images/logo.svg').default,
+    menu: require('~/assets/images/menu.svg').default,
 };
 export default images;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -3,16 +3,35 @@ import { useEffect, useState } from 'react';
 import Tippy from '@tippyjs/react/headless';
 import { IoMdCloseCircle } from 'react-icons/io';
 import { AiOutlineLoading3Quarters } from 'react-icons/ai';
-import { BiSearch } from 'react-icons/bi';
+import { BiSearch, BiHelpCircle } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
+import { GrLanguage } from 'react-icons/gr';
+import { RxKeyboard } from 'react-icons/rx';
 
 import Button from '~/components/Button';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
+import Menu from '~/components/Popper/Menu';
 
 const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+    {
+        icon: <GrLanguage />,
+        title: 'English',
+    },
+    {
+        icon: <BiHelpCircle />,
+        title: 'Feedback and help',
+        to: '/help',
+    },
+    {
+        icon: <RxKeyboard />,
+        title: 'Keyboard shortcut',
+    },
+];
 
 function Header() {
     const [searchResult, setSearchResult] = useState([]);
@@ -30,15 +49,15 @@ function Header() {
                     interactive
                     visible={searchResult.length > 0}
                     render={(attrs) => (
-                        <PopperWrapper>
-                            <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
                                 <h4 className={cx('search-title')}>Account</h4>
                                 <AccountItem />
                                 <AccountItem />
                                 <AccountItem />
                                 <AccountItem />
-                            </div>
-                        </PopperWrapper>
+                            </PopperWrapper>
+                        </div>
                     )}
                 >
                     <div className={cx('search')}>
@@ -52,12 +71,18 @@ function Header() {
                             <BiSearch className={cx('search-icon')} />
                         </button>
                     </div>
-                </Tippy>{' '}
+                </Tippy>
                 <div className={cx('actions')}>
                     <Button black leftIcon={<IoMdAdd />}>
                         Upload
                     </Button>
                     <Button primary>Log in</Button>
+
+                    <Menu items={MENU_ITEMS}>
+                        <button className={cx('more-btn')}>
+                            <img src={images.menu} alt="menu" className={cx('menu-icon')} />
+                        </button>
+                    </Menu>
                 </div>
             </div>
         </header>

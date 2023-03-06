0ee3d93551daa3322f269e3150c3f20188317e77
xhiep <xhhiep@gmail.com>
2023-03-06 16:08:25 +0700

    Xây dựng UI phần sidebar #1

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,7 +1,7 @@
 import classNames from 'classnames/bind';
 import Header from '~/layouts/components/Header';
 import styles from './DefaultLayout.module.scss';
-import Sidebar from './Sidebar';
+import Sidebar from '~/layouts/components/Sidebar';
 
 const cx = classNames.bind(styles);
 
---
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -0,0 +1,33 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+import Menu, { MenuItem } from './Menu';
+import config from '~/config';
+import {
+    HomeActiveIcon,
+    HomeIcon,
+    LiveActiveIcon,
+    LiveIcon,
+    UserActiveGroupIcon,
+    UserGroupIcon,
+} from '~/components/Icon';
+
+const cx = classNames.bind(styles);
+
+function Sidebar() {
+    return (
+        <aside className={cx('wrapper')}>
+            <Menu>
+                <MenuItem title="For You" to={config.routes.home} icon={<HomeIcon />} activeIcon={<HomeActiveIcon />} />
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserActiveGroupIcon />}
+                />
+                <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
+            </Menu>
+        </aside>
+    );
+}
+
+export default Sidebar;
